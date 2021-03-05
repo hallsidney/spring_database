c97657d740dafad87e035c2553fcec464779e6d2
SIDNEY HALL <sidney.hall@austincc.edu>
2021-03-05 15:27:06 -0600

    2C

--- src/main/java/com/example/MySQLDemo/controller/LessonController.java
+++ src/main/java/com/example/MySQLDemo/controller/LessonController.java
@@ -1,8 +1,9 @@
 package com.example.MySQLDemo.controller;
 import com.example.MySQLDemo.dao.LessonRepository;
 import com.example.MySQLDemo.model.Lesson;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/lessons")
@@ -22,6 +23,11 @@ public class LessonController {
     @PostMapping("")
     public Lesson create(@RequestBody Lesson lesson) {
         return this.repository.save(lesson);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Lesson> read(@PathVariable Long id) {
+        return this.repository.findById(id);
     }
 
 }
